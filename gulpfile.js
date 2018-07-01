1d1629d97dca961baeca741237f544c073bf0cd2
imroz <imrozmnr@gmail.com>
2018-07-01 18:13:38 +0530

    completed responsive chapter

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
+
+/*gulp.task('default',function(){
+	console.log("Hurray u have created a gulp task");
+});*/
+
+/*gulp.task('html', function(){
+	console.log("Imagine something usefull being done to your html here.");
+});*/
+
+
+
+
+
+
