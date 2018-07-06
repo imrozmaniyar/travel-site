77092cbd2a5e1e0f953bc86d6843e8ef37296377
imroz <imrozmnr@gmail.com>
2018-07-07 00:22:38 +0530

    Completed automatic sprite generation

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 require('./gulp/tasks/styles');
 require('./gulp/tasks/watch');
+require('./gulp/tasks/sprites');
 
 /*gulp.task('default',function(){
 	console.log("Hurray u have created a gulp task");
